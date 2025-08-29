469caf7c3a953294e4bfedf86d498f41726f1fcc
KashifHosein <kashifhosein@gmail.com>
2025-08-29 04:19:54 -0400

    Create move_zeros.cpp

---
+++ arrays/move_zeros.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums){
+        int l = 0, r = 0;
+
+        while(r < nums.size()){
+            if(nums[r] != 0){
+                int temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+                l+=1;
+            }
+            r+=1;
+        }
+    }
+};
